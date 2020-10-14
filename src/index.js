294ccec13cf1dff4ca65af888593d5c3c56e401c
tmg26 <59874692+tmg26@users.noreply.github.com>
2020-10-14 18:32:56 +0900

    画面をリセット

--- src/index.js
+++ src/index.js
@@ -6,13 +6,11 @@ import "mdbreact/dist/css/mdb.css"
 import Router from './container/Router.js'
 import { Provider } from './reducer/index'
 import ErrorBoundary from './container/ErrorBoundary'
+import App from './App'
 
 
 ReactDOM.render(
-    <ErrorBoundary>
-        <Provider>
-            <Router />
-        </Provider>
-    </ErrorBoundary>,
+    <APP>
+    </APP>,
     document.getElementById('root')
 )
