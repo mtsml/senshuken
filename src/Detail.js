9450fd4dd3eed197f78600a52f426415a86be951
matsu <50922910+mtsml@users.noreply.github.com>
2020-10-28 18:14:10 +0900

    Router追加

---
+++ src/Detail.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+
+class Detail extends React.Component {
+    render() {
+        return (
+            <>
+                detailです
+            </>
+        )
+    }
+}
+
+export default Detail
--- src/index.js
+++ src/index.js
@@ -1,13 +1,18 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import { BrowserRouter as Router, Route } from "react-router-dom";
 import "@fortawesome/fontawesome-free/css/all.min.css"
 import "bootstrap-css-only/css/bootstrap.min.css"
 import "mdbreact/dist/css/mdb.css"
 import "./css/senshuken.css"
 import App from './App'
+import Detail from './Detail'
 
 
 ReactDOM.render(
-    <App />,
+    <Router>
+        <Route exact path="/" component={App}></Route>
+        <Route exact path="/detail" component={Detail}></Route>
+    </Router>,
     document.getElementById('root')
 )
