24806675b7795b9dde7d422487c98d475ef8afd3
matsu <50922910+mtsml@users.noreply.github.com>
2020-11-15 11:52:39 +0900

    routerのファイル構成変更

--- src/index.js
+++ src/index.js
@@ -1,9 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import { BrowserRouter as Router, Route } from "react-router-dom";
 
 import App from './App'
-import Detail from './Detail'
 
 import "@fortawesome/fontawesome-free/css/all.min.css"
 import "bootstrap-css-only/css/bootstrap.min.css"
@@ -12,9 +10,6 @@ import "./css/senshuken.css"
 
 
 ReactDOM.render(
-    <Router>
-        <Route exact path="/" component={App}></Route>
-        <Route exact path="/detail/:id" component={Detail}></Route>
-    </Router>,
+    <App />,
     document.getElementById('root')
 )
