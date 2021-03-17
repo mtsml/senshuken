4e0bb475679e3b287fed506627020dcdcf66c7bd
two <72131214+yui-two@users.noreply.github.com>
2021-03-17 20:02:20 +0900

    ヘッダーロゴの修正

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,6 +3,7 @@ import React from 'react';
 const Header = () => (
   <header>
     <div className="logo">
+      <i class="far fa-grin-squint-tears"></i>
       <a className="link" href="/">senshuken</a>
     </div>
   </header>
