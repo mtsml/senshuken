c6fd5f128cb6d145153912a5d43388fd3e1d6af3
two <72131214+yui-two@users.noreply.github.com>
2021-03-24 19:55:45 +0900

    result画面更新

--- src/routes/Result/Result.jsx
+++ src/routes/Result/Result.jsx
@@ -3,29 +3,39 @@ import { MDBContainer } from 'mdbreact';
 
 const SENSHUKEN = [
   {
-    id: 1, question: 'クイズ1-1', answer: 'クイズ1-1の答え', senshuken_id: 1,
+    id: 1, question: 'クイズ1-1', answer: true, senshuken_id: 1,
   },
   {
-    id: 2, question: 'クイズ1-2', answer: 'クイズ1-2の答え', senshuken_id: 1,
+    id: 2, question: 'クイズ1-2', answer: true, senshuken_id: 1,
   },
   {
-    id: 3, question: 'クイズ1-3', answer: 'クイズ1-3の答え', senshuken_id: 1,
+    id: 3, question: 'クイズ1-3', answer: true, senshuken_id: 1,
   },
   {
-    id: 4, question: 'クイズ1-4', answer: 'クイズ1-4の答え', senshuken_id: 1,
+    id: 4, question: 'クイズ1-4', answer: true, senshuken_id: 1,
   },
   {
-    id: 5, question: 'クイズ1-5', answer: 'クイズ1-5の答え', senshuken_id: 1,
+    id: 5, question: 'クイズ1-5', answer: false, senshuken_id: 1,
   },
   {
-    id: 6, question: 'クイズ1-6', answer: 'クイズ1-6の答え', senshuken_id: 1,
+    id: 6, question: 'クイズ1-6', answer: false, senshuken_id: 1,
   },
 ];
+
+const calScore = (senshukens) => {
+  let trueCnt = 0;
+  for (let i = 0; i < senshukens.length; i++) {
+    if (senshukens[i].answer === true) {
+      trueCnt++;
+    }
+  }
+  return trueCnt;
+}
 
 const Result = () => (
   <MDBContainer className="text-center">
     <div>
-      {SENSHUKEN.map((senshuken2) => <p>{senshuken2.id}</p>)}
+      {calScore(SENSHUKEN)} / {SENSHUKEN.length}
     </div>
   </MDBContainer>
 );
